4b63bf072d281512cee03354a90616b518735873
Tia <104393269+tiadotdev@users.noreply.github.com>
2022-06-05 11:50:16 -0400

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+
+/**
+ * Gutenberg style editor
+ */
+add_action('wp_enqueue_scripts', 'tiadotdev_enqueue_theme_scripts');
+function tiadotdev_enqueue_theme_scripts() {
+    wp_enqueue_script( 'theme-editor', get_template_directory_uri() . '/assets/js/editor.js', array( 'wp-blocks', 'wp-dom' ), filemtime( get_template_directory() . '/assets/js/editor.js' ), true );
+}
